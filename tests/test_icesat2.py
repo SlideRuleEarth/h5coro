f646945cd58d4a50494743b612123f8921a6bc3d
JP Swinski <jp.swinski@nasa.gov>
2023-08-16 17:03:25 +0000

    updated pytests to latest interface

--- tests/test_icesat2.py
+++ tests/test_icesat2.py
@@ -10,23 +10,25 @@ ATL03_FILE = "/data/ATLAS/ATL03_20181017222812_02950102_005_01.h5"
 @pytest.mark.region
 class TestIcesat2:
     def test_s3driver(self):
-        h5obj = h5coro.H5Coro(ATL03_S3_OBJECT, s3driver.S3Driver, datasets=['/gt2l/heights/h_ph'])
-        assert h5obj['/gt2l/heights/h_ph'].elements == 20622551
-        assert h5obj['/gt2l/heights/h_ph'].datasize == 82490204
-        assert h5obj['/gt2l/heights/h_ph'].numrows  == 20622551
-        assert h5obj['/gt2l/heights/h_ph'].numcols == 1
-        assert abs(h5obj['/gt2l/heights/h_ph'].values[0] - 2553.0833) < 0.0001
+        h5obj = h5coro.H5Coro(ATL03_S3_OBJECT, s3driver.S3Driver)
+        promise = h5obj.readDatasets(['/gt2l/heights/h_ph'])
+        assert promise['/gt2l/heights/h_ph'].elements == 20622551
+        assert promise['/gt2l/heights/h_ph'].datasize == 82490204
+        assert promise['/gt2l/heights/h_ph'].numrows  == 20622551
+        assert promise['/gt2l/heights/h_ph'].numcols == 1
+        assert abs(promise['/gt2l/heights/h_ph'].values[0] - 2553.0833) < 0.0001
 
     def test_filedriver(self):
-        h5obj = h5coro.H5Coro(ATL03_FILE, filedriver.FileDriver, datasets=['/gt2l/heights/h_ph'])
-        assert h5obj['/gt2l/heights/h_ph'].elements == 20622551
-        assert h5obj['/gt2l/heights/h_ph'].datasize == 82490204
-        assert h5obj['/gt2l/heights/h_ph'].numrows  == 20622551
-        assert h5obj['/gt2l/heights/h_ph'].numcols == 1
-        assert abs(h5obj['/gt2l/heights/h_ph'].values[0] - 2553.0833) < 0.0001
+        h5obj = h5coro.H5Coro(ATL03_FILE, filedriver.FileDriver)
+        promise = h5obj.readDatasets(['/gt2l/heights/h_ph'])
+        assert promise['/gt2l/heights/h_ph'].elements == 20622551
+        assert promise['/gt2l/heights/h_ph'].datasize == 82490204
+        assert promise['/gt2l/heights/h_ph'].numrows  == 20622551
+        assert promise['/gt2l/heights/h_ph'].numcols == 1
+        assert abs(promise['/gt2l/heights/h_ph'].values[0] - 2553.0833) < 0.0001
 
     def test_inspect_variable(self):
-        h5obj = h5coro.H5Coro(ATL03_S3_OBJECT, s3driver.S3Driver, block=False)
+        h5obj = h5coro.H5Coro(ATL03_S3_OBJECT, s3driver.S3Driver)
         metadata, attributes = h5obj.inspectVariable('/gt2l/heights/h_ph', w_attr=True)
         assert metadata.dimensions[0] == 20622551
         assert attributes['/gt2l/heights/h_ph/units'].elements == 1
